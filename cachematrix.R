3ef4859ac35a5d5d589ee37bb7968213f43ee0c7
Ravi Shankar <sankarsana@gmail.com>
2014-11-20 15:28:02 +0530

    Implemented assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and their may be 
+# some benefit to caching the inverse of a matrix rather than compute 
+# it repeatedly. 
+##
 
-## Write a short comment describing this function
+##
+#This R script attempts to do just that using 2 functions:
+# makeCacheMatrix for caching of the matrix data and its inverse 
+# and cacheSolve for computing the inverse of matrix and using 
+# the cache to store the data
+##
+
+# Author: Ravi Shankar
+
+
+########################################################
+#This function creates a special "matrix" object that can 
+#cache its inverse.
+#######################################################
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    matrix_inverse <- NULL 
+    set <- function(y) {
+        x <<- y
+        matrix_inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) matrix_inverse <<- inverse
+    getinverse <- function() matrix_inverse
+    # Return a list that is used as a cached by the cacheSolve function
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##
+# This function computes the inverse of the special "matrix" returned by 
+# makeCacheMatrix above. If the inverse has already been calculated 
+# (and the matrix has not changed), then the cachesolve should retrieve 
+# the inverse from the cache.
+##
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    matrix_inverse <- x$getinverse()
+    if(!is.null(matrix_inverse)) {
+        #message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    matrix_inverse <- solve(data, ...)
+    x$setinverse(matrix_inverse)
+    matrix_inverse
 }
